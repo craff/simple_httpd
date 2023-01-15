e2b7f9f30a7dae8d2a078812d21f61c98eca0dac
craff <christophe@raffalli.eu>
2023-01-15 10:20:52 -1000

    fix chunked test now that tests/foo_50 is random

--- tests/chunked.sh
+++ tests/chunked.sh
@@ -14,37 +14,37 @@ echo download1 1>&2
 curl -N "http://localhost:${PORT}/foo_50" -o data \
   -H 'Tranfer-encoding: chunked' --max-time 10
 
-wc data
-wc data 1>&2
+wc -c data
+wc -c data 1>&2
 
 echo download2 1>&2
 curl -N "http://localhost:${PORT}/foo_50" -o data \
   -H 'Tranfer-encoding: chunked' --max-time 10
 
-wc data
+wc -c data
 
 echo download3 1>&2
 curl -N "http://localhost:${PORT}/foo_50" -o data \
   -H 'Tranfer-encoding: chunked' --max-time 10
 
-wc data
+wc -c data
 
 echo upload1 1>&2
 cat foo_50 | curl -N -X PUT http://localhost:$PORT/data --data-binary @- \
   -H 'Transfer-Encoding: chunked' --max-time 10 >\dev\null
 
-wc data
+wc -c data
 
 echo upload2 1>&2
 cat foo_50 | curl -N -X PUT http://localhost:$PORT/data --data-binary @- \
   -H 'Transfer-Encoding: chunked' --max-time 10 >\dev\null
 
-wc data
+wc -c data
 
 echo upload3 1>&2
 cat foo_50 | curl -N -X PUT http://localhost:$PORT/data --data-binary @- \
   -H 'Transfer-Encoding: chunked' --max-time 10 >\dev\null
 
-wc data
+wc -c data
 
 kill $PID
