997bca91f30d7d37b6d836ec95dffa4598346829
craff <christophe@raffalli.eu>
2023-01-15 12:07:38 -1000

    adjust tests

--- tests/stress-ssl.sh
+++ tests/stress-ssl.sh
@@ -5,16 +5,16 @@ PORT=8444
 "$SERVER" . -p $PORT --cache --ssl domain.crt domain.key > /dev/null &
 PID=$!
 
+# need to sleep to make sure server is ready before first curl
+sleep 2
+
 url=https://localhost:${PORT}/foo_50
 nb=$2
-sleep_time=0
 
 for (( c=1; c<=$nb; c++ )); do
   f=$(mktemp)
   (curl -k -s $url > $f; stat -c %s $f; rm $f) &
   PIDS[$c]=$!
-
-  sleep $sleep_time
 done
 
 #echo ${PIDS[@]}
--- tests/stress.sh
+++ tests/stress.sh
@@ -5,16 +5,16 @@ PORT=8088
 "$SERVER" . -p $PORT --cache &
 PID=$!
 
+# need to sleep to make sure server is ready before first curl
+sleep 2
+
 url=http://localhost:${PORT}/foo_50
 nb=$2
-sleep_time=0
 
 for (( c=1; c<=$nb; c++ )); do
   f=$(mktemp)
   (curl -s $url > $f; stat -c %s $f; rm $f) &
   PIDS[$c]=$!
-
-  sleep $sleep_time
 done
 
 #echo ${PIDS[@]}
