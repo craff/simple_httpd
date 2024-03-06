75eedbb1e99400d34337b9ddcb00d27be76b5f29
Christophe Raffalli <christophe@raffalli.eu>
2024-03-06 09:39:29 -1000

    sharing code in src/gen

--- src/gen/compile_headers_mli.ml
+++ src/gen/compile_headers_mli.ml
@@ -1,36 +1,4 @@
-
-let filename =
-  if Array.length Sys.argv <> 2 then
-    begin
-      Printf.eprintf "usage: %s path_to_field_names.csv" Sys.argv.(0);
-      exit 1
-    end;
-  Sys.argv.(1)
-
-let ch = open_in filename
-
-let _ = input_line ch
-
-let rec fn acc =
-  try
-    let line = input_line ch in
-    if String.length line > 0 && Char.lowercase_ascii line.[0] <> Char.uppercase_ascii  line.[0] then
-      begin
-        let line = String.split_on_char ',' line in
-        let line = List.map String.trim line in
-        fn (line :: acc)
-      end
-    else acc
-  with
-    End_of_file -> acc
-
-let to_cstr = String.map (function '-' -> '_' | c -> c)
-
-let filename = ["Filename-Multipart"; ""; ""; "Managment of filenames in multipart encoded data as a fake header"; ""]
-
-let lines = List.rev (fn [filename])
-
-let fields = List.map (function [] -> assert false | (h::_) -> h) lines
+open Compile_headers_common
 
 let _ =
   Printf.printf "type header =\n";
--- src/gen/compile_status_mli.ml
+++ src/gen/compile_status_mli.ml
@@ -1,39 +1,4 @@
-
-let filename =
-  if Array.length Sys.argv <> 2 then
-    begin
-      Printf.eprintf "usage: %s path_to_field_names.csv" Sys.argv.(0);
-      exit 1
-    end;
-  Sys.argv.(1)
-
-let ch = open_in filename
-
-let _ = input_line ch
-
-let rec fn acc =
-  try
-    let line = input_line ch in
-    if String.length line > 0 then
-      begin
-        let line = String.split_on_char ',' line in
-        let line = List.map String.trim line in
-        fn (line :: acc)
-      end
-    else acc
-  with
-    End_of_file -> acc
-
-let to_cstr = String.map (function ('-'|' ') -> '_' | c -> c)
-let to_lower s = String.lowercase_ascii (to_cstr s)
-
-let lines = List.rev (fn [])
-let lines = List.filter (function _::x::_ -> x <> "Unassigned"
-                         && String.index_opt x '('= None
-                        | _ -> false) lines
-let lines = List.map (function x::y::l -> x::y::String.concat " " l::[]
-                              | _ -> assert false) lines
-let fields = List.map Array.of_list lines
+open Compile_status_common
 
 let _ =
   Printf.printf "type t = private int\n";
