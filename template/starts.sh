f49d425ba3dfc7b6178973207cdb116d3cac4ce2
Christophe Raffalli <christophe@raffalli.eu>
2024-10-25 09:22:55 -1000

    create log folder when running template/starts.sh

--- template/starts.sh
+++ template/starts.sh
@@ -3,6 +3,11 @@
 # should use --top-dir if it is not started in the current folder
 # and needs access to the store. Here all file are small enough to
 # be kept in memory
+
+if [ ! -d log ]; then
+   mkdir log
+fi
+
 dune exec -- ./server.exe \
                 --log-folder ./log --restart-file session.save \
                 --ssl ../_build/default/tests/domain.crt ../_build/default/tests/domain.key
