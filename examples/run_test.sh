f835fa0c21d4593d558c3222463938bb185ee85b
craff <christophe@raffalli.eu>
2026-01-03 15:17:19 -1000

    A bit more robust times for sse test

--- examples/run_test.sh
+++ examples/run_test.sh
@@ -5,7 +5,8 @@ PORT=8082
 ./sse_server.exe -p $PORT --log-requests 0 --log-exceptions 0 &
 PID=$!
 
-sleep 0.1
+sleep 0.05 # server send every 0.1s and we expect 10 messages
+
 ./sse_client.exe -p $PORT --alarm=1 /count | tr -d '\r' || true
 
 kill $PID
